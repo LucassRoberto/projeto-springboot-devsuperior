9a031277c4be4a299281c2f168c25dfb656c5ba7
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-05-05 10:23:25 -0300

    @LucassRoberto Task: Atualizar usuário

--- src/main/java/com/projetosite/course/service/UserService.java
+++ src/main/java/com/projetosite/course/service/UserService.java
@@ -31,4 +31,19 @@ public class UserService {
 	public void delete (Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update (Long id, User obj) {
+		User entity = repository.getById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		
+	}
+
+	//apenas esses dados poderão ser atualizados (Opção)
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+	entity.setPhone(obj.getPhone());	
+	}
+	
 }
