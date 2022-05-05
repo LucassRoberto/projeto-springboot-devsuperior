5d3ddb5b8274ad3c0ed21e92884a4de866ff13f8
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-05-05 09:32:37 -0300

    @LucassRoberto Task: Inserindo novos usuários no banco de dados

--- src/main/java/com/projetosite/course/service/UserService.java
+++ src/main/java/com/projetosite/course/service/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert (User obj) {  //operação para inserir no banco de dados um novo objeto do tipo User
+		return repository.save (obj);
+	}
 }
